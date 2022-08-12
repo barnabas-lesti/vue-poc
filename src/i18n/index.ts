430674a406d780fe8a7441ad2ff0211d80281f2f
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-08-12 11:40:36 +0200

    Updated noScroll logic to use subscription

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,8 @@
 import * as VueI18n from 'vue-i18n';
 
-import { messages } from './messages';
+import messages from './messages';
 
-export const i18n = VueI18n.createI18n({
+export default VueI18n.createI18n({
   locale: 'en',
   fallbackLocale: 'en',
   messages,
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import ViewHome from '../views/ViewHome.vue';
 import ViewNotFound from '../views/ViewNotFound.vue';
 import ViewPosts from '../views/ViewPosts.vue';
 
-export const router = createRouter({
+export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
